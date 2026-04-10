48237b3a90f14ebd94d5c54cf3948f5d87b4715f
munai99 <80685868+munai99@users.noreply.github.com>
2026-04-10 15:20:23 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,13 @@
-const CACHE_NAME = 'image-launcher-v1';
-const urlsToCache = ['/', '/index.html'];
+const CACHE_NAME = 'gazou-app-v1';
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(['./', './index.html']))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
