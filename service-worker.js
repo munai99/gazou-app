d797a2d3cc3b514f7fb0df172fe86dae488be4d9
munai99 <80685868+munai99@users.noreply.github.com>
2026-04-10 16:30:58 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'fav-image-launcher-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
