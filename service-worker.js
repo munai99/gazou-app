5c0631e80f8690f210540246561c74316e8c9350
munai99 <80685868+munai99@users.noreply.github.com>
2026-04-10 16:37:35 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,7 @@ const CACHE_NAME = 'fav-image-launcher-v1';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', event => {
@@ -13,8 +11,6 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
